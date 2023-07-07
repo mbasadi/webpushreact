00530cdfc9a34fa3d6b63f3cda5ee8afd097af6d
mbasadi <asadi.mohammadbagher@gmail.com>
2023-07-07 14:26:04 -0400

    self

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-restricted-globals */
 // service-worker.js
-window.addEventListener('push', (event) => {
-  if (!(window.Notification && window.Notification.permission === 'granted')) {
+self.addEventListener('push', (event) => {
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
     return;
   }
   const data = event.data.json();
@@ -9,10 +10,8 @@ window.addEventListener('push', (event) => {
   const icon = data.icon;
   const tag = data.tag;
 
-  // Log the URL of each client this service worker controls
-
   event.waitUntil(
-    window.registration.showNotification(title, {
+    self.registration.showNotification(title, {
       body: message,
       tag,
       icon
