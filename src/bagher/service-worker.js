cc3b0b286a9dfc5056e5ffb490578754c30cf831
mbasadi <asadi.mohammadbagher@gmail.com>
2023-07-12 15:09:03 -0400

    move back

--- src/bagher/service-worker.js
+++ src/bagher/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-window.addEventListener('push', (event) => {
-  if (!(window.Notification && window.Notification.permission === 'granted')) {
+self.addEventListener('push', (event) => {
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
     return;
   }
   const data = event.data.json();
@@ -12,7 +12,7 @@ window.addEventListener('push', (event) => {
   // Log the URL of each client this service worker controls
 
   event.waitUntil(
-    window.registration.showNotification(title, {
+    self.registration.showNotification(title, {
       body: message,
       tag,
       icon
