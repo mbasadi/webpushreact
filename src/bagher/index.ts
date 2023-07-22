bc6c8cedb2f941ed10e08bc9e6c36cff31088871
mbasadi <asadi.mohammadbagher@gmail.com>
2023-07-22 10:28:37 -0400

    update

--- src/bagher/index.ts
+++ src/bagher/index.ts
@@ -229,7 +229,7 @@ class NotificationAPIClient implements NotificationAPIClientInterface {
   askForWebPushPermission = (): void => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-        .register('/bagher.js')
+        .register('/service-worker.js')
         .then(async (registration) => {
           Notification.requestPermission().then(async (permission) => {
             if (permission === 'granted') {
