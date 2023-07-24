db050da72ccb1ec8765593b5e2e9b4f570df7586
mbasadi <asadi.mohammadbagher@gmail.com>
2023-07-23 22:03:47 -0400

    importScripts

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-undef
-mportScripts('https://unpkg.com/notificationapi-js-client-sdk/dist/service-worker.js');
+importScripts('https://unpkg.com/notificationapi-js-client-sdk/dist/service-worker.js');
